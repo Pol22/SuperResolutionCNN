7a58d84098beb6f2fe39676f62a676c0c36efa9c
Pavel Iliutchenko <p.iliutchenk@samsung.com>
2019-01-18 17:26:17 +0300

    Add some code

--- create_dataset.py
+++ create_dataset.py
@@ -4,7 +4,9 @@ import os.path
 
 
 IMAGE_DIR = 'images'
+SAVE_DIR = 'dataset'
 IMG_SIZE = 256
+LOW_RES = 128
 
 
 images = os.listdir(IMAGE_DIR)
@@ -15,6 +17,20 @@ for image in images:
     width, height = img.size
     for i in range(width // IMG_SIZE - 1):
         for j in range(height // IMG_SIZE - 1):
-            crop_img = img.crop((i*IMG_SIZE, j*IMG_SIZE,
-                (i+1)*IMG_SIZE - 1, (j+1)*IMG_SIZE - 1))
+            ref_img = img.crop((i*IMG_SIZE, j*IMG_SIZE,
+                (i+1)*IMG_SIZE, (j+1)*IMG_SIZE))
+            resized_img = ref_img.resize(
+                (LOW_RES, LOW_RES))
+            bicubic_img = resized_img.resize(
+                (IMG_SIZE, IMG_SIZE), Image.BICUBIC)
+            crop_name = image.split('.')[:-1]
+            crop_name.append(str(i))
+            crop_name.append(str(j))
+            crop_name.append('.jpg')
+            crop_name = ''.join(crop_name) 
+            bicubic_path = os.path.join(SAVE_DIR, 'bicubic', crop_name)
+            ref_path = os.path.join(SAVE_DIR, 'reference', crop_name)
+            ref_img.save(ref_path)
+            bicubic_img.save(bicubic_path)
+            
             
--- srcnn.py
+++ srcnn.py
@@ -1,5 +1,9 @@
 from keras.models import Sequential
 from keras.layers import Conv2D
+import os
+from PIL import Image
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 BATCH_SIZE = 10
@@ -19,6 +23,49 @@ model.add(Conv2D(n1, kernel_size=f1, activation='relu',
 model.add(Conv2D(n2, kernel_size=f2, activation='relu', padding='same'))
 model.add(Conv2D(NUM_CHANELS, kernel_size=f3, padding='same'))
 
-model.compile(optimizer='adam', loss='mean_squared_error')
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
 print(model.summary())
+
+# load 
+bicubic_imgs = os.listdir('dataset/bicubic')
+dataset = []
+for img in bicubic_imgs:
+    img_path = os.path.join('dataset/bicubic', img)
+    dataset.append(np.asarray(Image.open(img_path), dtype=np.float32))
+
+bicubic_dataset = np.asarray(dataset)
+
+ref_imgs = os.listdir('dataset/reference')
+dataset = []
+for img in ref_imgs:
+    img_path = os.path.join('dataset/reference', img)
+    dataset.append(np.asarray(Image.open(img_path), dtype=np.float32))
+
+ref_dataset = np.asarray(dataset)
+print("Bicubic dataset shape:", bicubic_dataset.shape)
+print("Reference dataset shape:", ref_dataset.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    bicubic_dataset, ref_dataset,
+    test_size=0.2, random_state=12444, shuffle=True)
+
+print(X_test.shape)
+print(X_train.shape)
+
+# model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5)
+# model.save('srcnn.h5')
+from keras.models import load_model
+model = load_model('srcnn.h5')
+x = 232
+sr_75 = model.predict(np.reshape(X_test[x,:,:,:], (1, IMG_SIZE, IMG_SIZE, NUM_CHANELS)))
+sr_75 = np.asarray(sr_75, dtype=np.uint8)
+sr_75 = np.squeeze(sr_75)
+img_75 = Image.fromarray(sr_75)
+img_75.save('res_75.jpg')
+test_75 = Image.fromarray(X_test[x].astype(np.uint8))
+test_75.save('test_75.jpg')
+ref_75 = Image.fromarray(y_test[x].astype(np.uint8))
+ref_75.save('ref_75.jpg')
+
+
